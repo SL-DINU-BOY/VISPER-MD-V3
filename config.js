d0fecb403450bfa376795d7a4b74a559e4e0fea8
SL-DINU-BOY <139786264+SL-DINU-BOY@users.noreply.github.com>
2025-08-21 16:46:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'PUT SESSION ID HERE' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&FIghWaDB#QamrDBlr7iKyOSMCt0zsOQ-1ft_3HhlU9wfQUxV0hMU' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
